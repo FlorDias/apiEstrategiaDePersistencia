99ba78401ccf4bd12420838026c40749a03407c5
FlorDias <flordias1806@gmail.com>
2023-05-03 20:19:45 -0300

    agregado de materias sin migracion

--- api/app.js
+++ api/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var carrerasRouter = require('./routes/carreras');
+var materiaRouter = require('./routes/materia');
 
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/car', carrerasRouter);
+app.use('/mat', materiaRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
---
+++ api/routes/materia.js
@@ -0,0 +1,82 @@
+var express = require("express");
+var router = express.Router();
+var models = require("../models");
+
+router.get("/", (req, res) => {
+  console.log("Esto es un mensaje para ver en consola");
+  models.materia
+    .findAll({
+      attributes: ["id", "nombre", "id_carrera"]
+    })
+    .then(materias => res.send(materias))
+    .catch(() => res.sendStatus(500));
+});
+
+router.post("/", (req, res) => {
+  models.materia
+    .create({ nombre: req.body.nombre, id_carrera: req.body.id_carrera  })
+    .then(materias => res.status(201).send({ id: materias.id }))
+    .catch(error => {
+      if (error == "SequelizeUniqueConstraintError: Validation error") {
+        res.status(400).send('Bad request: existe otra carrera con el mismo nombre')
+      }
+      else {
+        console.log(`Error al intentar insertar en la base de datos: ${error}`)
+        res.sendStatus(500)
+      }
+    });
+});
+
+const findMateria = (id, { onSuccess, onNotFound, onError }) => {
+  models.materia
+    .findOne({
+      attributes: ["id", "nombre", "id_carrera"],
+      where: { id }
+    })
+    .then(materias => (materias ? onSuccess(materias) : onNotFound()))
+    .catch(() => onError());
+};
+
+router.get("/:id", (req, res) => {
+  findMateria(req.params.id, {
+    onSuccess: materias => res.send(materias),
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const onSuccess = carrera =>
+    carrera
+      .update({ nombre: req.body.nombre, id_carrera: req.body.id_carrera }, { fields: ["nombre", "id_carrera"] })
+      .then(() => res.sendStatus(200))
+      .catch(error => {
+        if (error == "SequelizeUniqueConstraintError: Validation error") {
+          res.status(400).send('Bad request: existe otra carrera con el mismo nombre')
+        }
+        else {
+          console.log(`Error al intentar actualizar la base de datos: ${error}`)
+          res.sendStatus(500)
+        }
+      });
+    findMateria(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  const onSuccess = materias =>
+    materias
+      .destroy()
+      .then(() => res.sendStatus(200))
+      .catch(() => res.sendStatus(500));
+    findMateria(req.params.id, {
+    onSuccess,
+    onNotFound: () => res.sendStatus(404),
+    onError: () => res.sendStatus(500)
+  });
+});
+
+module.exports = router;
